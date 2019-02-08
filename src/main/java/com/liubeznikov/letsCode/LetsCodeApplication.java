db085aa2371dbf9d1eaba0a3ed36b0aea79c4c68
Nikolay Liubeznikov <12liubeznikov@mail.ru>
2019-02-08 15:41:15 +0300

    1st commit

---
+++ src/main/java/com/liubeznikov/letsCode/LetsCodeApplication.java
@@ -0,0 +1,14 @@
+package com.liubeznikov.letsCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetsCodeApplication.class, args);
+	}
+
+}
+
